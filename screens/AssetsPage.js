9fc0d88c22d18ae3f84a344a11dec35eba01f552
melissacskn <melissacoskun@gmail.com>
2024-06-04 19:16:41 +0300

    assetsPage is modified

--- screens/AssetsPage.js
+++ screens/AssetsPage.js
@@ -1,10 +1,46 @@
 
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet,FlatList } from 'react-native';
+import { Alert, StatusBar, ImageBackground,StyleSheet,View, Text,FlatList,TouchableOpacity,Button} from "react-native";
 import { signOut, getCurrentUser, fetchAuthSession } from 'aws-amplify/auth';
+import styled from 'styled-components';
+
+import AntDesign from "react-native-vector-icons/AntDesign";
+import Post from '../components/post';
+
+import {
+  StyledContainer,
+  InnerContainer,
+  PageLogo,
+  PageTitle,
+  SubTitle,
+  StyledFormArea,
+  StyledButton,
+  ButtonText,
+  WelcomeContainer,
+  Colors,
+  AssetTitle,
+  TenantTitle,
+  Container,
+  LeftIcon,
+  RightCornerContainer
+
+  
+} from '../components/styles4';
+// Colors
+const { darkLight, primary, green,black } = Colors;
 
 const AssetsPage = ({ route }) => {
-  const { itemId, itemName } = route.params;
+  const [nameArray,setNameArray]=useState([])
+  const [nana,setNana]=useState('')
+  const [status,setStaus]=useState([])
+  const [str,setstr]=useState("")
+
+  const[typeAssest,setTypeAsset]=useState([])
+
+
+  
+
+const { itemId, itemName } = route.params;
  const [count,setCount]=useState(0)
 
       //GETTING ASSETS OF AN INDIVUAL TENANT
@@ -21,44 +57,133 @@ const AssetsPage = ({ route }) => {
     };
     const responseee = await fetch(`https://api.dev.nonprod.civic.ly/assets/${itemId}/asset/`,requestOptionsss)
     const jsonnn = await responseee.json();
-     console.log("Response JSON:", jsonnn);
+    console.log("Response JSON:", jsonnn);
     setCount(jsonnn.count)
+    let ar=[]
+    let sta=[]
+    let assetTypes=[]
+    let uplo=[]
      for(let i=0; i<count;i++){
 
-      console.log("Asset Name: ",jsonnn.results[i].name)
+      ar.push(jsonnn.results[i].name)
+      sta.push(jsonnn.results[i].status)
+      assetTypes.push (jsonnn.results[i].asset_type.name)
+      uplo.push(jsonnn.results[i].asset_uploads.map(item => item.file));
+    
 
     }
+
+const urls = jsonnn.results[0].asset_uploads.map(item => item.file);
+console.log(urls[0]);  // Access the second URL using its index
+setstr(urls);
+console.log(str)
+
+
+    
+    setNameArray(ar)
+    setStaus(sta)
+    setTypeAsset(assetTypes)
+
+    console.log(nameArray[0])
+    console.log(typeAssest[0])
+    console.log(typeof(nameArray[0]))
+    
+    // setNana(nameArray[0])
+
+  
+
+   
     
      
 }
+const exampleData = {
+  image: "https://images.pexels.com/photos/736230/pexels-photo-736230.jpeg?cs=srgb&dl=pexels-jonaskakaroto-736230.jpg&fm=jpg",
+  assetType: typeAssest[0],
+  name: nameArray[0],
+  status: status[0],
+  postImage:"https://images.pexels.com/photos/736230/pexels-photo-736230.jpeg?cs=srgb&dl=pexels-jonaskakaroto-736230.jpg&fm=jpg",
+};
+
 useEffect(() => {
     getAssets()
   }, []); 
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Details Screen</Text>
-      <Text>Id: {itemId}</Text>
-      <Text>Organisation Name: {itemName}</Text>
-      <Text>Number of asset {count}</Text>
-     
-    </View>
-  );
-};
 
+
+  return (
+
+    <>
+    <ImageBackground
+      source={require('./../assets/Pulse-mobile.png')}
+      resizeMode='cover'
+      style={styles.container}
+      imageStyle={styles.image}
+    >
+    
+        <StatusBar backgroundColor='transparent'
+            translucent={true}
+            />
+            
+   
+      <InnerContainer>
+      <PageLogo
+          resizeMode="cover"
+          source={require('./../assets/images/civicly-remove.png')}
+        />
+        <WelcomeContainer>
+      <StyledFormArea>
+       
+      <Container>
+        <RightCornerContainer>
+          <LeftIcon>
+            <AntDesign name='user' size={23} color='green' />
+          </LeftIcon>
+          <TenantTitle>{itemName}</TenantTitle>
+        </RightCornerContainer>
+        <AssetTitle>Assets</AssetTitle>
+        <Post
+          image={exampleData.image}
+          name={exampleData.name}
+          assetType={exampleData.assetType}
+          status={exampleData.status}
+          postImage={exampleData.postImage}
+        />
+      </Container>
+  
+    </StyledFormArea>
+    </WelcomeContainer>
+    </InnerContainer>
+  
+      </ImageBackground>
+    </>
+  );
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: 'black',
     alignItems: 'center',
-    padding: 16,
+    justifyContent: 'center'
   },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
+  image: {
+    opacity: 0.7
+  }
 });
+
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     padding: 16,
+//   },
+//   title: {
+//     fontSize: 24,
+//     fontWeight: 'bold',
+//     marginBottom: 10,
+//   },
+// });
 
 
 
